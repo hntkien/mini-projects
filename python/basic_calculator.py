1bb541da3415e9ea99a0d26722e070d86bf52b51
KikiHN <116190130+hntkien@users.noreply.github.com>
2023-11-03 19:27:23 +0800

    Modify basic_calculator.py
    Remove if-else, change the options to dictionary

--- python/basic_calculator.py
+++ python/basic_calculator.py
@@ -7,59 +7,68 @@
 # 5. While loop to contiue until the user want to exit 
 
 ##### Feature Functions #####
+
+# Addition
 def add(a, b):
     print("Addition")
     result = a + b 
     print(f"{a} + {b} = {result}\n")
-    
+
+# Subtraction    
 def sub(a, b):
     print("Subtraction")
     result = a - b 
     print(f"{a} - {b} = {result}\n") 
 
+# Multiplication
 def mul(a, b):
     print("Multiplication")
     result = a * b 
     print(f"{a} * {b} = {result}\n") 
 
+# Division
 def div(a, b):
     print("Division")
     result = a / b 
     print(f"{a} / {b} = {result}\n") 
 
-##### Main Function #####
-def main():
-
+# Function to display available options
+def display_choices():
     print("A. Addition")
     print("B. Subtraction")
     print("C. Multiplication")
     print("D. Division")
     print("E. Exit")
-    valid_choices = ['A', 'B', 'C', 'D', 'E']
+    available_operations = {
+        'A': add,
+        'B': sub,
+        'C': mul,
+        'D': div
+    }
+    return available_operations 
+
+##### Main Function #####
+def main():
 
     while True:
 
+        available_operations = display_choices()
         choice = input("Input your choice for operation: ").upper()
-        # Continue with the next loop if choice is nto a valid options
-        if choice not in valid_choices:
-            print("Invalid option.")
-            continue 
+
         # Terminate the program immediately if user enter keyword 'E' 
-        elif choice == 'E':
+        if choice == 'E':
             break 
+        # Continue with the next loop if choice is nto a valid options
+        elif choice not in available_operations.keys():
+            print("Invalid option.\n")
+            continue
+        else:
+            operation = available_operations[choice]
 
         a = int(input("Enter the first number: "))
         b = int(input("Enter the second number: "))
-        
-        # Conditions 
-        if choice == 'A':
-            add(a, b) 
-        elif choice == 'B':
-            sub(a, b)
-        elif choice == 'C':
-            mul(a, b) 
-        elif choice == 'D':
-            div(a, b) 
+        # Run the selected function with parameters a, b
+        operation(a, b)
     
     print("Program Terminated.")
 
